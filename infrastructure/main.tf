7b1e187504a902f652fefc072ca1bfb9d1dedfd8
briandenicola <brian@briandenicola.com>
2022-02-17 12:33:48 -0600

    Added keyvault to infra deployment

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,9 +11,13 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
-locals {
-    location            = "southcentralus"
-    resource_group_name = "MSI_Todo_Testing_RG"
+resource "random_id" "this" {
+  byte_length = 2
+}
+
+resource "random_pet" "this" {
+  length = 1
+  separator  = ""
 }
 
 resource "random_password" "password" {
@@ -21,19 +25,84 @@ resource "random_password" "password" {
   special = true
 }
 
+locals {
+    location                    = "southcentralus"
+    certificate_base64_encoded  = filebase64("${path.module}/my-wildcard-cert.pfx")
+    certificate_password        = "...................."
+    resource_name               = "${random_pet.this.id}-${random_id.this.dec}"
+}
+
 resource "azurerm_resource_group" "this" {
-  name                  = local.resource_group_name
+  name                  = "${local.resource_name}_rg"
   location              = local.location
 }
 
 resource "azurerm_user_assigned_identity" "this" {
-  name                = "my-demo-identity"
+  name                = "${local.resource_name}-identity"
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
 }
 
+resource "azurerm_key_vault" "this" {
+  name                        = "${local.resource_name}-kv"
+  resource_group_name         = azurerm_resource_group.this.name
+  location                    = azurerm_resource_group.this.location
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  enable_rbac_authorization   = true
+
+  sku_name = "standard"
+
+  network_acls {
+    bypass                    = "AzureServices"
+    default_action            = "Allow"
+  }
+}
+
+resource "azurerm_key_vault_certificate" "this" {
+  name         = "my-wildcard-pfx-cert"
+  key_vault_id = azurerm_key_vault.this.id
+
+  certificate {
+    contents = local.certificate_base64_encoded
+    password = local.certificate_password
+  }
+
+  certificate_policy {
+    issuer_parameters {
+      name = "Self"
+    }
+
+    key_properties {
+      exportable = true
+      key_size   = 2048
+      key_type   = "RSA"
+      reuse_key  = false
+    }
+
+    secret_properties {
+      content_type = "application/x-pkcs12"
+    }
+  }
+}
+
+resource "azurerm_role_assignment" "secrets" {
+  scope                = azurerm_key_vault.this.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_user_assigned_identity.this.principal_id
+  skip_service_principal_aad_check = true  
+}
+
+resource "azurerm_role_assignment" "certs" {
+  scope                = azurerm_key_vault.this.id
+  role_definition_name = "Key Vault Certificates Officer"
+  principal_id         = azurerm_user_assigned_identity.this.principal_id
+  skip_service_principal_aad_check = true  
+}
+
 resource "azurerm_virtual_network" "this" {
-  name                = "demo-identity-network"
+  name                = "${local.resource_name}-network"
   address_space       = ["10.5.0.0/16"]
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
@@ -47,7 +116,7 @@ resource "azurerm_subnet" "this" {
 }
 
 resource "azurerm_network_interface" "this" {
-  name                = "myvm001-nic"
+  name                = "${local.resource_name}-nic"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 
@@ -59,7 +128,7 @@ resource "azurerm_network_interface" "this" {
 }
 
 resource "azurerm_linux_virtual_machine" "this" {
-  name                = "myvm001"
+  name                = "${local.resource_name}-vm"
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
   size                = "Standard_B2ms"
@@ -95,7 +164,7 @@ resource "azurerm_linux_virtual_machine" "this" {
 }
 
 resource "azurerm_mssql_server" "this" {
-  name                         = "mysqlserver001"
+  name                         = "${local.resource_name}-sql"
   resource_group_name          = azurerm_resource_group.this.name
   location                     = local.location
   version                      = "12.0"

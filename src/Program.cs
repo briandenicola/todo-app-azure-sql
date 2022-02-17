e81a54676b1cb3b4f1fdb030e370f2a4ddf1129e
briandenicola <brian@briandenicola.com>
2022-02-17 12:06:11 -0600

    Added https handling from a Key Vault

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var keyVaultClient = new CertificateClient(new Uri("https://myvault.vault.azure.net/"), new DefaultAzureCredential());
+var keyVaultCertificateX509 = (await keyVaultClient.DownloadCertificateAsync("my-wildcard-pfx-cert")).Value;
+
+int port = 443;
+builder.WebHost.UseKestrel( opt => 
+{
+    opt.ConfigureHttpsDefaults( httpsOptions => 
+    {
+        httpsOptions.SslProtocols = System.Security.Authentication.SslProtocols.Tls13;
+    });
+    opt.Listen(IPAddress.Any, port, listenOptions =>
+    {
+        listenOptions.UseHttps(keyVaultCertificateX509);
+    });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
@@ -19,5 +35,6 @@ builder.Services.AddDbContext<TodoDbContext>(
 );
 
 var app = builder.Build();
+
 app.MapControllers();
 app.Run();

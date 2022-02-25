e82e744729999b1b1b33c1e2a179f6cbb319f254
briandenicola <brian@briandenicola.com>
2022-02-25 10:18:35 -0600

    Fixed issue with role assignment to network

--- infrastructure/workload-identity-example/main.tf
+++ infrastructure/workload-identity-example/main.tf
@@ -205,6 +205,12 @@ resource "null_resource" "this" {
   }
 }
 
+resource "azurerm_role_assignment" "aks" {
+  scope                = azurerm_virtual_network.this.id
+  role_definition_name = "Network Contributor"
+  principal_id         = azurerm_kubernetes_cluster.this.identity[0].principal_id
+}
+
 resource "azurerm_mssql_server" "this" {
   name                         = "${local.resource_name}-sql"
   resource_group_name          = azurerm_resource_group.this.name

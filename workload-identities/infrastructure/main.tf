444d1b47f1caf01b256790ba8f338237678fbef2
Brian Denicola <brian@briandenicola.com>
2022-04-27 08:05:52 -0500

    Moved Microsoft Defender to TF provider

--- workload-identities/infrastructure/main.tf
+++ workload-identities/infrastructure/main.tf
@@ -96,7 +96,6 @@ resource "azurerm_resource_group" "this" {
   }
 }
 
-
 resource "azurerm_key_vault" "this" {
   name                        = "${local.resource_name}-kv"
   resource_group_name         = azurerm_resource_group.this.name
@@ -244,55 +243,11 @@ resource "azurerm_kubernetes_cluster" "this" {
     log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
   }
 
+  microsoft_defender {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.k8s.id
+  }
+
 }
-
-resource "azurerm_resource_group_template_deployment" "this" {
-  depends_on = [
-    azurerm_kubernetes_cluster.this
-  ]
-
-  name                = "post-cluster-setup"
-  resource_group_name = azurerm_resource_group.this.name
-  deployment_mode     = "Incremental"
-  parameters_content  = jsonencode({
-    "aksCluster"      = {
-      value = local.aks_name
-    },
-    "logAnalyticsId"  = {
-      value = azurerm_log_analytics_workspace.this.id
-    }
-  })
-  template_content = <<TEMPLATE
-    {
-      "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
-      "contentVersion": "1.0.0.0",
-      "parameters": {
-        "aksCluster": {
-          "type": "string"
-        },
-        "logAnalyticsId": {
-          "type": "string"
-        }
-      },
-      "resources": [
-        { 
-          "type": "Microsoft.ContainerService/managedClusters", 
-          "apiVersion": "2021-07-01", 
-          "name": "[parameters('aksCluster')]", 
-          "location": "[resourceGroup().location]",
-          "properties": {
-            "securityProfile": { 
-              "azureDefender": { 
-                "enabled": true, 
-                "logAnalyticsWorkspaceResourceId": "[parameters('logAnalyticsId')]"
-              }
-            }
-          }
-        }
-      ]
-    }
-TEMPLATE
-} 
 
 resource "azurerm_role_assignment" "aks" {
   scope                = azurerm_virtual_network.this.id

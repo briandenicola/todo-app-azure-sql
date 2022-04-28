4fb94e2b18236c9344ce03f26250004d04d03f97
Brian Denicola <brian@briandenicola.com>
2022-04-28 06:59:39 -0500

    Fixed API version for azapi_update_resource

--- pod-identities/infrastructure/main.tf
+++ pod-identities/infrastructure/main.tf
@@ -7,8 +7,12 @@ terraform {
     }
     azapi = {
       source = "Azure/azapi"
+      version = "0.1.1"
     }
   }
+}
+
+provider "azapi" {
 }
 
 provider "azurerm" {
@@ -228,14 +232,13 @@ resource "azapi_update_resource" "this" {
     azurerm_kubernetes_cluster.this
   ]
 
-  type        = "Microsoft.ContainerService/managedClusters@2022-03-01"
+  type        = "Microsoft.ContainerService/managedClusters@2021-07-01"
   resource_id = azurerm_kubernetes_cluster.this.id
 
   body = jsonencode({
     properties = {
       podIdentityProfile = {
         enabled = true
-        userAssignedIdentities = []
       }
     }
   })

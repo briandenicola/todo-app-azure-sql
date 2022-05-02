18feb210e8d42b0946d67f02d8520b12a4dc07f5
briandenicola <brian@briandenicola.com>
2022-05-02 09:39:44 -0500

    Bug fix for code directory

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd ../managed-identities/src
+cd ../pod-identities/src
 dotnet publish -c Release -r linux-x64 --self-contained --nologo -o publish/linux -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true

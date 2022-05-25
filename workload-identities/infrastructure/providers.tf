63ef517626136226153a78d3158daef33b0462a0
Brian <brian@briandenicola.com>
2022-05-25 08:38:02 -0500

    Update providers.tf

--- workload-identities/infrastructure/providers.tf
+++ workload-identities/infrastructure/providers.tf
@@ -29,21 +29,6 @@ provider "helm" {
     client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_key)
     username               = azurerm_kubernetes_cluster.this.kube_config.0.username
     password               = azurerm_kubernetes_cluster.this.kube_config.0.password
-
-    /*exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
-      args        = [
-        "get-token", 
-         "--environment", 
-        "AzurePublicCloud",
-        "--server-id", 
-        "6dae42f8-4368-4678-94ff-3960e28e3630", 
-       "--client-id", 
-        "80faf920-1908-4b52-b5ef-a8e7bedfc67a",
-        "--tenant-id",
-        data.azurerm_client_config.current.tenant_id
-     ]
-      command     = "kubelogin"
-    }*/
+    
   }
 }

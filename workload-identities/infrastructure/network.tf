902c1d2eb08a2b447338ebfcb885aaedc3672f9b
briandenicola <brian@briandenicola.com>
2022-07-05 07:40:28 -0500

    Moved to unique range for environment

--- workload-identities/infrastructure/network.tf
+++ workload-identities/infrastructure/network.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "this" {
   name                = "${local.resource_name}-network"
-  address_space       = ["10.5.0.0/16"]
+  address_space       = ["10.27.0.0/16"]
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 }
@@ -9,7 +9,7 @@ resource "azurerm_subnet" "this" {
   name                 = "servers"
   resource_group_name  = azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
-  address_prefixes     = ["10.5.2.0/24"]
+  address_prefixes     = ["10.27.2.0/24"]
 }
 
 resource "azurerm_network_security_group" "this" {

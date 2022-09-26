77408603e87f781c4e0688f664f6abe89c8f3f59
briandenicola <brian@briandenicola.com>
2022-09-26 14:02:47 -0500

    Provider Version bump

--- workload-identities/infrastructure/providers.tf
+++ workload-identities/infrastructure/providers.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm  = {
       source = "hashicorp/azurerm"
-      version = "3.3.0"
+      version = "3.24.0"
     }
     azuread = {
       source = "hashicorp/azuread"
@@ -11,7 +11,7 @@ terraform {
     }
     helm = {
       source = "hashicorp/helm"
-      version = "2.5.1"
+      version = "2.6.0"
     }
   }
 }

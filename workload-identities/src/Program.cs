2fcb7ce179570573f377189c3c41870ada855767
briandenicola <brian@briandenicola.com>
2022-10-03 13:13:12 -0500

    Branch to work on issue #3

--- workload-identities/src/Program.cs
+++ workload-identities/src/Program.cs
@@ -1,6 +1,7 @@
 Uri keyVaultUri;
 IConfigurationRoot config;
 string appInsights;
+SqlConnectionStringBuilder connection;
 
 {
     var switchMappings = new Dictionary<string, string>()
@@ -17,13 +18,14 @@ string appInsights;
     config = builder.Build();
     keyVaultUri = Helpers.GetKeyVaultUri(config["keyvault"]);
     appInsights = config["appinsights"];
+
+    connection = Helpers.BuildAzureConnectionString(config["azuresql"], clientid: config["clientid"]);
 }
 
 {   
     var builder = WebApplication.CreateBuilder();
 
     builder.AddCustomApplicationInsightsConfiguration(appInsights);
-    builder.AddCustomSQLAuthentication(config["azuresql"]);
     
     if( keyVaultUri is not null ) {
         await builder.AddCustomKeyVaultConfiguration(keyVaultUri);
@@ -31,7 +33,13 @@ string appInsights;
 
     builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
-    
+
+    builder.Services.AddDbContext<TodoDbContext>(    
+        options => options.UseSqlServer(
+            connection.ConnectionString
+        )
+    );
+
     var app = builder.Build();
     app.Logger.LogInformation("Application is ready to run."); 
     app.MapControllers();

d91baa6eb427411353ef82b92097dc0ec30aa3f5
briandenicola <brian@briandenicola.com>
2022-10-03 11:27:05 -0500

    Updated federation script for Managed Identity

--- scripts/workload-identity.sh
+++ scripts/workload-identity.sh
@@ -3,18 +3,23 @@
 while (( "$#" )); do
   case "$1" in
     -c|--cluster-name)
-      CLUSTER_NAME+=($2)
+      CLUSTER_NAME=$2
       shift 2
       ;;
     -n|--namespace)
       NAMESPACE=$2
       shift 2
       ;;
+    -g|--resource-group)
+      RG=$2
+      shift 2
+      ;;
     -h|--help)
       echo "Usage: ./workload-identity.sh --cluster-name --namespace
         Overview: This script will federate an Azure AD SPN with a Kubernetes Service Account
-        --cluster-name(c) - The AKS cluster where this identity will be used
-        --namespace(n)    - The Kuberentes namespace where this identity will be used
+        --cluster-name(c)   - The AKS cluster where this identity will be used
+        --namespace(n)      - The Kuberentes namespace where this identity will be used
+        --resource-group(g) - The Resource Group where the Managed Identity lives
       "
       exit 0
       ;;
@@ -36,25 +41,35 @@ AZURE_TENANT_ID=`echo ${CLUSTER_DETAILS} | jq -r ".[].identity.tenantId"`
 OIDC_FEATURE_ENABLED=`az aks show --resource-group ${CLUSTER_RG} --name ${CLUSTER_NAME} --query "oidcIssuerProfile.enabled" -o tsv`
 if [ ${OIDC_FEATURE_ENABLED} == "false" ]; then
   echo OIDC Feature NOT enabled... Updating Cluster to enable.
-  az aks update -g ${CLUSTER_RG} -n ${CLUSTER_NAME} --enable-oidc-issuer
+  az aks update -g ${CLUSTER_RG} -n ${CLUSTER_NAME} --enable-oidc-issuer --enable-workload-identity
 fi
    
 SERVICE_ACCOUNT_NAME=${CLUSTER_NAME}-${NAMESPACE}-identity
 SERVICE_ACCOUNT_ISSUER=`az aks show --resource-group ${CLUSTER_RG} --name ${CLUSTER_NAME} --query "oidcIssuerProfile.issuerUrl" -o tsv`
-APPLICATION_CLIENT_ID=`az ad sp list --display-name ${SERVICE_ACCOUNT_NAME} -o tsv --query "[0].appId"`
-APPLICATION_OBJECT_ID=`az ad app show --id "${APPLICATION_CLIENT_ID}" --query objectId -o tsv`
 
-cat <<EOF > body.json
-{
-  "name": "kubernetes-federated-identity",
-  "issuer": "${SERVICE_ACCOUNT_ISSUER}",
-  "subject": "system:serviceaccount:${NAMESPACE}:${SERVICE_ACCOUNT_NAME}",
-  "description": "Kubernetes service account federated identity",
-  "audiences": [
-    "api://AzureADTokenExchange"
-  ]
-}
-EOF
+# Non managed Identity steps
+#APPLICATION_CLIENT_ID=`az ad sp list --display-name ${SERVICE_ACCOUNT_NAME} -o tsv --query "[0].appId"`
+#APPLICATION_OBJECT_ID=`az ad app show --id "${APPLICATION_CLIENT_ID}" --query objectId -o tsv`
 
-az rest --method POST --uri "https://graph.microsoft.com/beta/applications/${APPLICATION_OBJECT_ID}/federatedIdentityCredentials" --body @body.json
-rm -rf body.json
+#cat <<EOF > body.json
+# {
+#  "name": "kubernetes-federated-identity",
+#  "issuer": "${SERVICE_ACCOUNT_ISSUER}",
+#  "subject": "system:serviceaccount:${NAMESPACE}:${SERVICE_ACCOUNT_NAME}",
+#  "description": "Kubernetes service account federated identity",
+#  "audiences": [
+#    "api://AzureADTokenExchange"
+#  ]
+# }
+#EOF
+
+#az rest --method POST --uri "https://graph.microsoft.com/beta/applications/${APPLICATION_OBJECT_ID}/federatedIdentityCredentials" --body @body.json
+#rm -rf body.json
+
+SUBJECT="system:serviceaccount:${NAMESPACE}:${SERVICE_ACCOUNT_NAME}"
+az identity federated-credential create \
+  --name ${SERVICE_ACCOUNT_NAME} \
+  --identity-name ${SERVICE_ACCOUNT_NAME} \
+  --resource-group ${RG} \
+  --issuer ${SERVICE_ACCOUNT_ISSUER} \
+  --subject ${SUBJECT}

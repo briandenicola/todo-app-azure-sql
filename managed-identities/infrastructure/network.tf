092716be3ff44db9fc94dc79a5b2c02f3bc09d54
briandenicola <brian@briandenicola.com>
2024-06-20 14:13:08 -0500

    ⚽ Thu Jun 20 14:13:08 CDT 2024

--- managed-identities/infrastructure/network.tf
+++ managed-identities/infrastructure/network.tf
@@ -49,6 +49,30 @@ resource "azurerm_network_security_group" "this" {
   name                = "${local.resource_name}-nsg"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
+
+    security_rule {
+    name                       = "bastion-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_address_prefix      = "168.63.129.16"
+    source_port_range          = "*"
+    destination_address_prefix = "*"
+    destination_port_range     = "22"
+  }
+
+  security_rule {
+    name                       = "bastion-rdp"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_address_prefix      = "168.63.129.16"
+    source_port_range          = "*"
+    destination_address_prefix = "*"
+    destination_port_range     = "3389"
+  }
 }
 
 resource "azurerm_subnet_network_security_group_association" "this" {
